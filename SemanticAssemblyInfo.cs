34093fae18681b2a4e7ab02858241ca1c8e30816
Bryan Apellanes <bryan@threeheadz.com>
2021-02-19 18:51:16 -0800

    TryGetPath for BamSettings

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("678f035")]
-[assembly: AssemblySemanticVersion("1.0.5-test+678f035")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+678f035, Revision=1.0.5.2096251931")]
+[assembly: AssemblyCommit("836ac51")]
+[assembly: AssemblySemanticVersion("1.0.5-test+836ac51")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+836ac51, Revision=1.0.5.3248583363")]
 [assembly: AssemblyProduct("bamd")]
