5c8e05c9aaba1db6458ae405bb20bf4d7b5a8da1
Bryan Apellanes <bryan@threeheadz.com>
2021-04-03 10:48:45 -0700

    minor test refactor and cleanup

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("836ac51")]
-[assembly: AssemblySemanticVersion("1.0.5-test+836ac51")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+836ac51, Revision=1.0.5.3248583363")]
+[assembly: AssemblyCommit("d41aff2")]
+[assembly: AssemblySemanticVersion("1.0.5-test+d41aff2")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+d41aff2, Revision=1.0.5.4063944801")]
 [assembly: AssemblyProduct("bamd")]
