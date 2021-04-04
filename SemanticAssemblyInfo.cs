0e6ccc1532ece5d46c41909b3153da32d43e2132
Bryan Apellanes <bryan@threeheadz.com>
2021-04-03 17:39:16 -0700

    set build output in projects to \opt\bam\build

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("d41aff2")]
-[assembly: AssemblySemanticVersion("1.0.5-test+d41aff2")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+d41aff2, Revision=1.0.5.4063944801")]
+[assembly: AssemblyCommit("5c8e05c")]
+[assembly: AssemblySemanticVersion("1.0.5-test+5c8e05c")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+5c8e05c, Revision=1.0.5.1944732174")]
 [assembly: AssemblyProduct("bamd")]
